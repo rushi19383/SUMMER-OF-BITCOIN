34fe21f4e264f0e83a92d5ee1e12a06e217738a3
Rushikesh Kantule <rushikesh19383@gmail.com>
2021-06-14 14:38:25 +0530

    first

---
+++ SOB.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+string input = "mempool.csv"; 
+set<string> included_trx_ids; // Will store all tx_ids
+int totalBlockWeight = 4000000; // The total weight of transactions in a block must not exceed 4,000,000 weight
+
+class TransactionInfo{
+    public:
+    string tx_id;
+    int fee;
+    int weight;
+    vector<string> parents;
+};
+
+pair<string,TransactionInfo*> initiateTransaction(vector<string>& row){
+    auto ans = new TransactionInfo();
+    ans->tx_id=row[0];
+    ans->fee=stoi(row[1]);
+    ans->weight= stoi(row[2]);
+    vector<string> p;
+    for (int i = 3; i < row.size(); i++)
+    {       
+        p.push_back(row[i]);
+    }
+    ans->parents=p;
+    return {row[0],ans};
+    
+}
