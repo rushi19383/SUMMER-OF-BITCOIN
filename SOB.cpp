0d7277d07b39f2728c413dae6bb48c069c760c87
Rushikesh Kantule <rushikesh19383@gmail.com>
2021-06-14 15:01:03 +0530

    main remaining

--- SOB.cpp
+++ SOB.cpp
@@ -31,4 +31,40 @@ pair<string,TransactionInfo*> initiateTransaction(vector<string>& row){
 
 void readinput(string fileName,unordered_map<string,TransactionInfo*>& ump){
     ifstream fin(fileName);
+    vector<string> row;
+    string temp,line,word;
+    getline(fin,line);
+    while (getline(fin,line)){
+        row.clear();
+        stringstream s(line);
+        while (getline(s,word,','))
+        {
+            row.push_back(word);
+
+        }
+        pair<string, TransactionInfo*>p=initiateTransaction(row);
+        ump[p.first]=p.second;
+        
+    }
+    fin.close();
+    cout<<"num of transactions read :"<<ump.size()<<endl;
+    
+    
+}
+//This only works when if it contains parents
+bool isValidTx(TransactionInfo* tx,set<string>& included_tx_set){
+    for(auto parent : tx->parents){
+        if(included_tx_set.find(parent)== included_tx_set.end())
+            return false;
+
+
+    }
+    return true;
+}
+
+void writeOutput(vector<string>& included_tx_vector, string fn){
+    ofstream myfile(fn);
+    for(auto s:included_tx_vector)
+    myfile<<s<<endl;
+    myfile.close();
 }
